0e7f4700b657148c0e1708e6f3cdafccaa84ec1b
Gustavo <guvono4@gmail.com>
2022-08-17 18:53:54 -0300

    Iniciando projeto

---
+++ src/context/AppProvider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppContext from './AppContext';
+
+function AppProvider({ children }) {
+  
+  return (
+    <AppContext.Provider
+    value={}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export default AppProvider;
